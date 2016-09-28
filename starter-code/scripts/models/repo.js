5db434b911d625f766341a093aa969fba57e46e8
Bhavya Bharti <bhavyab@outlook.com>
2016-09-27 17:39:57 -0700

    populated the repos.allRepos and call the cllback function

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -2,21 +2,22 @@
   var repos = {};
 
   repos.allRepos = [];
-// TODO: create a githubToken.js file that we can use to generate our headers
+// DONE: create a githubToken.js file that we can use to generate our headers
          // properly.
-  $.ajax({
-    url: 'https://api.github.com/users/bhavyaab/repos',
-    type: 'GET',
-    headers: {'Authorization': 'token '+githubToken},
-    success: function(data, message, xhr){
-      
-    },
-  });
-  repos.requestRepos = function(callback) {
-    /* TODO: How would you like to fetch your repos? Someone say AJAX?!
-      Do not forget to call the callback! */
-  };
 
+  repos.requestRepos = function(callback) {
+    /* DONE: How would you like to fetch your repos? Someone say AJAX?!
+      Do not forget to call the callback! */
+    $.ajax({
+      url: 'https://api.github.com/users/bhavyaab/repos',
+      type: 'GET',
+      headers: {'Authorization': 'token '+githubToken},
+      success: function(data, message, xhr){
+        repos.allRepos = data;
+        callback();
+      },
+    });
+  };
   repos.withTheAttribute = function(myAttr) {
     /* NOTE: This Model method filters the full repos collection based
         on a particular attribute. For example, you could use this
